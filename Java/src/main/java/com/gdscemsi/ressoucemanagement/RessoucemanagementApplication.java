7992fa739994a485e203d217801ae156cdea4085
Yassine <myelattar99@gmail.com>
2024-01-13 11:37:22 +0100

    Initialise Spring

---
+++ Java/src/main/java/com/gdscemsi/ressoucemanagement/RessoucemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.gdscemsi.ressoucemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessoucemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RessoucemanagementApplication.class, args);
+	}
+
+}
